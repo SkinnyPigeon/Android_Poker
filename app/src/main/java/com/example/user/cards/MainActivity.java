1db7ab8a417cb1f83148684187788ebd33ab081e
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-08-19 09:56:51 +0100

    MainActivity and activity_main set up

---
+++ app/src/main/java/com/example/user/cards/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.user.cards;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 19/08/2016.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
