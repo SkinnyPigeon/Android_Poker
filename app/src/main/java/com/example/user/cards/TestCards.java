90366bddbc95545458dd524096af118bd1346862
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-08-19 09:58:38 +0100

    TestCards class started

---
+++ app/src/main/java/com/example/user/cards/TestCards.java
@@ -0,0 +1,35 @@
+package com.example.user.cards;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 19/08/2016.
+ */
+public class TestCards {
+    private String mDealtCard;
+    private ArrayList< String > mCards;
+    private ArrayList< String > mShuffledCards;
+
+    public TestCards() {
+        mCards = new ArrayList< String>();
+        mCards.add( "A♠" );
+        mCards.add( "A♥" );
+        mCards.add( "A♣" );
+        mCards.add( "A♦" );
+        mCards.add( "K♠" );
+        mCards.add( "Q♠" );
+        mCards.add( "K♥" );
+        mCards.add( "2♥" );
+        mCards.add( "3♥" );
+        mCards.add( "4♥" );
+        mCards.add( "5♥" );
+        mCards.add( "6♥" );
+        mCards.add( "J♠" );
+        mCards.add( "10♠" );
+        mCards.add( "9♠" );
+        mCards.add( "8♠" );
+        mCards.add( "7♠" );
+        mCards.add( "6♠" );
+
+    }
+}
